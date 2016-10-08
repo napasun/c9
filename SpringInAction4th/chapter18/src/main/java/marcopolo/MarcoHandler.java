3b565954fbeeacd0a112bf415fcca019f38ae624
khj <wkzkfmxk23@gmail.com>
2016-10-08 09:32:31 +0900

    18장

---
+++ SpringInAction4th/chapter18/src/main/java/marcopolo/MarcoHandler.java
@@ -0,0 +1,33 @@
+package marcopolo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+public class MarcoHandler extends AbstractWebSocketHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(MarcoHandler.class);
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		logger.info("Received message: " + message.getPayload());
+		Thread.sleep(2000);
+		session.sendMessage(new TextMessage("Polo!"));
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("연결 종료");
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("연결 시작");
+	}
+	
+	
+	
+}
