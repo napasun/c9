13068c0b65955ee56506b5fb0ad4a11ef28fa4e6
napasun <wkzkfmxk23@gmail.com>
2017-03-24 14:28:28 +0900

    Update NewDate.java
    주석 설명 추가

--- Java_Study/src/study/java/newdate/NewDate.java
+++ Java_Study/src/study/java/newdate/NewDate.java
@@ -34,12 +34,16 @@ public class NewDate {
 		//zoneld();
 		datetimeformatter();
 	}
-	
+	/**
+	 * Instant
+	 * */
 	static void Instant() {		//절대시간
 		Instant instant = Instant.now();	//영국 그라니치 천문대 시간
 		System.out.println(instant);
 	}
-	
+	/**
+	 * Duration
+	 * */
 	static void duration () {	// LocalDateTime을 사용, 시간의 차이
         Duration oneHours = Duration.ofHours(1);
         System.out.println(oneHours.getSeconds());	//3600
@@ -63,7 +67,9 @@ public class NewDate {
         System.out.println(duration.toDays());	//781
         System.out.println(duration.toHours());	//18753
 	}
-	
+	/**
+	 * Period
+	 * */
 	static void period() {	//LocalDate에서 사용
         Period tenDays = Period.ofDays(10);
         System.out.println(tenDays.getDays()); //10
@@ -87,7 +93,9 @@ public class NewDate {
         System.out.println(period2.getMonths());	//1
         System.out.println(period2.getDays());		//9
 	}
-	
+	/**
+	 * ChronoUnit
+	 * */
 	// 시간 비교
 	// 얼마나 차이가 나는 지 Duration 연산과 비슷하지만 년, 원, 주까지 비교 가능하다.
 	static void chronoUnit() {
@@ -130,7 +138,9 @@ public class NewDate {
 			931549598000000000 nano
          * */
 	}
-	
+	/**
+	 * LocalDate
+	 * */
 	static void localDatePrint() {	//좀 더 보편적 값으로 출력된다.
 		Date date = new Date();
 		System.out.println(date); // Sun Jan 01 00:00:00 KST 2017
@@ -138,7 +148,6 @@ public class NewDate {
 		LocalDate localdate = LocalDate.now();
 		System.out.println(localdate);	// 2017-01-01
 	}
-	
 	// 기존 Date를 초기화 할 경우 년도에는 1900년을 기준으로 이후 년도를 더한 값 넣고 월에는 -1을 한 월을 넣어야 했다.
 	// LocalDate에서는 좀 더 명시적으로 날짜를 초기화 할 수 있다.
 	static void localDateInit() {	
@@ -149,7 +158,6 @@ public class NewDate {
 		//LocalDate.of(2017, Month.FEBRUARY, 1);
 		System.out.println(localdate);
 	}
-	
 	// 기존의 날짜 연산의 경우 Calendar 객체를 사용해야 했다.
 	// LocalDate에서는 plus(Days|Months|Weeks|Years)를 제공한다.
 	// 연산 메서드를 사용할 경우 원본을 유지하고 새로운 값을 리턴한다.
@@ -164,14 +172,15 @@ public class NewDate {
 		System.out.println(localdate);	// 2017-02-01, 기존 데이터를 유지한다.
 		System.out.println(localdate.plusDays(10));	// 2017-02-11
 	}
-	
 	// 기타등등
 	static void localDateMethod () {
 		LocalDate localdate = LocalDate.of(2017, 2, 1);
 		System.out.println(localdate.getDayOfMonth());	//1
 		System.out.println(localdate.getDayOfYear());	//32
 	}
-	
+	/**
+	 * LocalTime
+	 * */
 	static void localTimeInit () {
 		LocalTime localtime1 = LocalTime.of(1, 2);
 		System.out.println(localtime1);	// 01:02
@@ -194,7 +203,9 @@ public class NewDate {
 		
 		System.out.println(localtime1.minusMinutes(1));	//01:01
 	}
-	
+	/**
+	 * LocalDateTime
+	 * */
 	static void localDateTimeInit() {
 		LocalDate localdate = LocalDate.of(2017, 2, 1);
 		LocalTime localtime = LocalTime.of(1, 2);
@@ -202,8 +213,9 @@ public class NewDate {
 		LocalDateTime localDateTime = LocalDateTime.of(localdate, localtime);
 		System.out.println(localDateTime);	// 2017-02-01T01:02
 	}
-	
-	
+	/**
+	 * Zoned
+	 * */
 	static void zoneld() {
 		ZonedDateTime zoned = ZonedDateTime.of(2017,  1, 1, 1, 1, 0, 0, ZoneId.of("America/New_York"));
 		ZonedDateTime zoned2 = ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("America/New_York"));
@@ -211,8 +223,9 @@ public class NewDate {
 		//다른 나라의 시간값을 가져오는 예제
 		System.out.println(Instant.now().atZone(ZoneId.of("America/New_York")));
 	}
-	
-	
+	/**
+	 * DateTimeFormatter
+	 * */
 	static void datetimeformatter() {
 		LocalDateTime localDateTime = LocalDateTime.of(2017, 2, 1, 1, 2);
 		
