f9e8ef439cc283ce701f9322959874c5c82e1b47
wkzkfmxk12 <wkzkfmxk12@naver.com>
2017-04-19 22:57:08 +0900

    Basictype

--- BasicType.ts
+++ BasicType.ts
@@ -26,3 +26,26 @@ let c: Color = Color.Green;
 
 enum Color2 {Red = 1, Green, Blue};
 let c2: Color2 = Color2.Green;
+
+enum Color3 {Red = 1, Green = 2, Blue = 4}
+let c3: Color3 = Color3.Green;
+
+//Any : 어떤 유형이든 사용할 수 있음
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false; // okay, definitely a boolean
+// 배열에 여러 유형을 혼합하여 사용할 수 있다.
+let anyList: any[] = [1, true, "free"];
+anyList[1] = 100;
+
+//void 값을 반환하지 않는 함수유형에 사용
+function warnUser(): void {
+    alert("This is my warning message");
+}
+//undefined 또는 null을 넣을 수 있다.
+let unusable: void = undefined;
+//unusable = 1;
+
+//Null, Undefined
+let u: undefined = undefined;
+let n: null = null;
