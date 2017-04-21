71a1ba0e0aa2d109b9c91ec9325800fd032ffa20
wkzkfmxk23 <wkzkfmxk23@gmail.com>
2017-04-21 17:44:26 +0900

    클래스

--- 04.Classes.ts
+++ 04.Classes.ts
@@ -8,6 +8,7 @@ JavaScript 프로그래머는이 객체 지향 클래스 기반 접근 방식을
  빌드 할 수 있습니다. TypeScript에서는 개발자가이 기술을 사용하고 다음 JavaScript 버전을
   기다릴 필요없이 모든 주요 브라우저와 플랫폼에서 작동하는 JavaScript로 컴파일 할 수 있습니다.
  */
+///constructor : 생성자
 class Greeter {
     greeting: string;
     constructor(message: string) {
@@ -19,3 +20,45 @@ class Greeter {
 }
 
 let greeter = new Greeter("world");
+
+///상속
+// extends 키워드로 하위 클래스를 만든 다.
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+/// Public, private, and protected modifiers
+// 기본은 public
+// 읽기 전용 readonly
+
+///Accessors 
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
