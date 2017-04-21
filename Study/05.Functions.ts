924cd7dfa6b13f2d122d02d037b61675e073b6e5
wkzkfmxk12 <wkzkfmxk12@naver.com>
2017-04-22 08:37:03 +0900

    1

--- Study/05.Functions.ts
+++ Study/05.Functions.ts
@@ -12,7 +12,7 @@ let result3 = buildName("Bob", "Adams");
 }
 
 
-function s452()  {
+namespace s452  {
 // '?' 을 사용해서 선택적으로 값을 넣고 안 넣을 수도 있다.
 function buildName(firstName: string, lastName?: string) {
     if (lastName)
@@ -25,3 +25,16 @@ let result1 = buildName("Bob");
 let result2 = buildName("Bob", "Adams", "Sr.");  // error
 let result3 = buildName("Bob", "Adams");
 
+}
+
+
+namespace s453 {
+// 파라미터 기본값을 지정할 수 있다.
+function a(b=2) {
+    return b;
+}
+
+a();
+a(3);
+
+}
