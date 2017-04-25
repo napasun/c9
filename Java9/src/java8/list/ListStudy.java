7d7f66a8df95616d180b0735a0a20f147ba8ebe7
wkzkfmxk23 <wkzkfmxk23@gmail.com>
2017-04-25 18:05:08 +0900

    java8 forEach

--- Java9/src/java8/list/ListStudy.java
+++ Java9/src/java8/list/ListStudy.java
@@ -3,10 +3,20 @@ package java8.list;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ListStudy {
 	public static void main(String[] args) {
 		List l = new ArrayList();
 		List constList = Collections.unmodifiableList(l);
+
+		//java forEach 
+		l.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 }
