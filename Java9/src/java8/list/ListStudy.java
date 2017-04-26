f5b5f298a7df5e8f3853fc93fce508ebc05efe90
wkzkfmxk23 <wkzkfmxk23@gmail.com>
2017-04-26 17:50:51 +0900

    forEach 관련 소스 추가

--- Java9/src/java8/list/ListStudy.java
+++ Java9/src/java8/list/ListStudy.java
@@ -7,16 +7,26 @@ import java.util.function.Consumer;
 
 public class ListStudy {
 	public static void main(String[] args) {
-		List l = new ArrayList();
-		List constList = Collections.unmodifiableList(l);
-
-		//java forEach 
-		l.forEach(new Consumer<String>() {
+		List<String> list = new ArrayList<String>();
+		
+		//수정 불가능한 리스트 만들기
+		List<String> constList = Collections.unmodifiableList(list);
+		
+		
+		//java8 이전 컬렉션 반복실행 
+		for(String str : list) {
+			System.out.println(str);
+		}
+		
+		//java8 컬렉션 반복 : forEach 
+		list.forEach(new Consumer<String>() {
 			@Override
 			public void accept(String arg0) {
-				// TODO Auto-generated method stub
-				
+				System.out.println(arg0);
 			}
 		});
+		
+		//람다식을 사용해서 코드 줄이기
+		list.forEach((str) -> System.out.println(str));
 	}
 }
