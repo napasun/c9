f3eade9f91edab60e5fe918e91f0816600c540ef
estgames <wkzkfmxk23@gmail.com>
2017-12-08 15:59:02 +0900

    study12_08

---
+++ uiTextField/uiTextField/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  uiTextField
+//
+//  Created by estgames on 2017. 12. 8..
+//  Copyright © 2017년 estgames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func tabView(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true
+        
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // 텍스트필드, 입력된 범위, 새로운 문자열
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let tmpStr = textField.text! as NSString
+        let replacedString = tmpStr.replacingCharacters(in: range, with: string)
+        
+        if replacedString == "" {
+            myLabel.text = "0"
+        } else {
+            if let num = Int(replacedString) {
+                myLabel.text = String(num * 25)
+            }
+        }
+        return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        myLabel.text = "0"
+        return true
+    }
+}
+
