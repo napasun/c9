c44eed12b3e58b140bcfc47f21813e63be5878e6
estgames <wkzkfmxk23@gmail.com>
2017-12-14 14:54:17 +0900

    폼UI 쓰기 소스

--- Memo/Memo/AppDelegate.swift
+++ Memo/Memo/AppDelegate.swift
@@ -13,6 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var memolist = [MemoData]()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
---
+++ Memo/Memo/MemoCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemoCell.swift
+//  Memo
+//
+//  Created by estgames on 2017. 12. 14..
+//  Copyright © 2017년 estgames. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    
+
+}
