49bbd9cc21a40246eba13e62d59d4a0e2a25d50f
estgames <wkzkfmxk23@gmail.com>
2017-12-18 13:29:00 +0900

    커스터 마이징 소스코드로 UI만들고 액션 연결

--- Customizing/InputForm/InputForm/ViewController.swift
+++ Customizing/InputForm/InputForm/ViewController.swift
@@ -9,12 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var paramEmail: UITextField!
+    var paramUpdate: UISwitch!
+    var paramInterval: UIStepper!
+    var txtUpdate: UILabel!
+    var txtInterval: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = "설정"    //네비게이션 바 타이틀
+        
+        //레이블 설정
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblEmail)
+        
+        //레이블 설정
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        //레이블 설정
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
+        
+        //이메일 텍스트 필드
+        self.paramEmail = UITextField()
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 13)
+        self.paramEmail.borderStyle = UITextBorderStyle.line
+        self.paramEmail.autocapitalizationType = .none  //첫글자 대문자로 나오는 거 끄기
+        
+        self.view.addSubview(self.paramEmail)
+        
+        //스위치
+        self.paramUpdate = UISwitch()
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        //스테퍼
+        self.paramInterval = UIStepper()
+        
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0
+        self.paramInterval.maximumValue = 100
+        self.paramInterval.stepValue = 1
+        self.paramInterval.value = 0
+        
+        self.view.addSubview(self.paramInterval)
+        
+        //스위치 상태를 표시할 레이블
+        self.txtUpdate = UILabel()
+        
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 12)
+        self.txtUpdate.textColor = UIColor.red
+        self.txtUpdate.text = "갱신함"
+        
+        self.view.addSubview(self.txtUpdate)
+        
+        //스테퍼 값을 표시할 레이블
+        self.txtInterval = UILabel()
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.font = UIFont.systemFont(ofSize: 12)
+        self.txtInterval.textColor = UIColor.red
+        self.txtInterval.text = "0분마다"
+        
+        self.view.addSubview(self.txtInterval)
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
     }
-
+    //스위치와 상호작용
+    @objc func presentUpdateValue(_ sender: UISwitch) {
+        self.txtUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
+    //스테퍼와 상호작용
+    @objc func presentIntervalValue(_ sender: UIStepper) {
+        self.txtInterval.text = ("\( Int(sender.value)) 분마다")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
