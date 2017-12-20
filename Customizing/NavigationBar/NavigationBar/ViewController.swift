fb884a6dbb269c31ce7807c0fc344c005b1afa80
estgames <wkzkfmxk23@gmail.com>
2017-12-20 14:50:57 +0900

    커스텀 네비게이션

---
+++ Customizing/NavigationBar/NavigationBar/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by estgames on 2017. 12. 20..
+//  Copyright © 2017년 estgames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.initTitleNew()
+        self.initTitleImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func initTitleImage() {
+        
+    }
+    
+    func initTitleNew() {   //커스텀 네이게이션 컨테이너에 두개의 레이블을 넣어 구현
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
+        
+        let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
+        topTitle.numberOfLines = 1
+        topTitle.textAlignment = .center
+        topTitle.font = UIFont.systemFont(ofSize: 15)
+        topTitle.textColor = UIColor.white
+        topTitle.text = "첫번째 줄"
+        
+        let subTitle = UILabel(frame: CGRect(x: 0, y: 18, width: 200, height: 18))
+        subTitle.numberOfLines = 1
+        subTitle.textAlignment = .center
+        subTitle.font = UIFont.systemFont(ofSize: 12)
+        subTitle.textColor = UIColor.white
+        subTitle.text = "두번째 줄"
+        
+        containerView.addSubview(topTitle)
+        containerView.addSubview(subTitle)
+        
+        self.navigationItem.titleView = containerView
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
+    
+    func initTitle() {  //하나의 레이블을 네이게이션 아이템에 넣어 구현
+        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        
+        nTitle.numberOfLines = 2
+        nTitle.textAlignment = .center
+        nTitle.textColor = UIColor.white
+        nTitle.font = UIFont.systemFont(ofSize: 15)
+        nTitle.text = "첫번째 줄 \n 두번째 줄"
+        
+        self.navigationItem.titleView = nTitle
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
+
+}
+
