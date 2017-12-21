9c49c8b48ee37eeae344e72870c9c14d197df516
estgames <wkzkfmxk23@gmail.com>
2017-12-21 11:31:40 +0900

    Alert창에 입력창 커스텀

---
+++ Customizing/Alert/Alert/ControlViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ControlViewController.swift
+//  Alert
+//
+//  Created by estgames on 2017. 12. 21..
+//  Copyright © 2017년 estgames. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+
+    let slider = UISlider()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.slider.minimumValue = 0
+        self.slider.maximumValue = 100
+        
+        self.slider.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
+        self.view.addSubview(self.slider)
+        
+        self.preferredContentSize = CGSize(width: self.slider.frame.width, height: self.slider.frame.height+10)
+    }
+}
