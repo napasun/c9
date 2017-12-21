4a3b3717ea6f7884d6c887d01d6300da7303df37
estgames <wkzkfmxk23@gmail.com>
2017-12-21 14:01:00 +0900

    얼랏창 리스트뷰

--- Customizing/Alert/Alert/MapAlertViewController.swift
+++ Customizing/Alert/Alert/MapAlertViewController.swift
@@ -42,6 +42,34 @@ class MapAlertViewController: UIViewController {
         sliderBtn.addTarget(self, action: #selector(sliderAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(sliderBtn)
+        
+        
+        let listBtn = UIButton(type: .system)
+        listBtn.frame = CGRect(x: 0, y: 300, width: 100, height: 30)
+        listBtn.center.x = self.view.frame.width / 2
+        listBtn.setTitle("List Alert", for: .normal)
+        listBtn.addTarget(self, action: #selector(listAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(listBtn)
+    }
+    
+    func didSelectRowAt(indexPath: IndexPath) {
+        print(">>> 선택된 행은 \(indexPath.row) 입니다.")
+    }
+    
+    @objc func listAlert(_ sender:Any) {
+        let contentVC = ListViewController()
+        //델리게이트를 자신으로 지정
+        contentVC.delegate = self
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        
+        alert.setValue(contentVC, forKeyPath: "contentViewController")
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: false)
     }
     
     @objc func sliderAlert(_ sender: Any) {
