030b78c1e676c4d9ff8d492fbf2baf022950930a
estgames <wkzkfmxk23@gmail.com>
2017-12-22 12:36:32 +0900

    커스텀 버튼

--- Customizing/CSButton/CSButton/CSButton.swift
+++ Customizing/CSButton/CSButton/CSButton.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
+public enum CSButtonType {
+    case rect
+    case circle
+}
+
 class CSButton: UIButton {
+    var style: CSButtonType = .rect {
+        didSet {
+            switch style {
+                case .rect :
+                    self.backgroundColor = UIColor.black
+                    self.layer.borderColor = UIColor.black.cgColor
+                    self.layer.borderWidth = 2
+                    self.layer.cornerRadius = 0
+                    self.setTitleColor(UIColor.white, for: .normal)
+                    self.setTitle("Rect Button", for: .normal)
+                case .circle :
+                    self.backgroundColor = UIColor.red
+                    self.layer.borderColor = UIColor.blue.cgColor
+                    self.layer.borderWidth = 2
+                    self.layer.cornerRadius = 50
+                    self.setTitle("Circle Button", for: .normal)
+            }
+        }
+    }
 
     required init?(coder aDecoder: NSCoder) {   //스토리보드 방식으로 객체를 생성할 때 호출되는 메소드
         super.init(coder: aDecoder)
@@ -31,6 +55,34 @@ class CSButton: UIButton {
     init() {
         super.init(frame:CGRect.zero)
     }
+    
+    convenience init(type: CSButtonType) {
+        self.init()
+        
+        switch type {
+            case .rect :
+                self.backgroundColor = UIColor.black
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0
+                self.setTitleColor(UIColor.white, for: .normal)
+                self.setTitle("Rect Button", for: .normal)
+            case .circle :
+                self.backgroundColor = UIColor.red
+                self.layer.borderColor = UIColor.blue.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 50
+                self.setTitle("Circle Button", for: .normal)
+        }
+        
+        self.addTarget(self, action: #selector(counting(_:)), for: .touchUpInside)
+    }
+    
+    @objc func counting(_ sender: UIButton) {
+        sender.tag = sender.tag + 1
+        sender.setTitle("\(sender.tag) 번째 클릭", for: .normal)
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- Customizing/CSButton/CSButton/ViewController.swift
+++ Customizing/CSButton/CSButton/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
         let frame = CGRect(x: 30, y: 50, width: 150, height: 30)
         let csBtn = CSButton(frame: frame)
         self.view.addSubview(csBtn)
+        
+        let rectBtn = CSButton(type: .rect)
+        rectBtn.frame = CGRect(x: 30, y: 200, width: 150, height: 30)
+        self.view.addSubview(rectBtn)
+        
+        let circleBtn = CSButton(type: .circle)
+        circleBtn.frame = CGRect(x: 200, y: 200, width: 150, height: 30)
+        self.view.addSubview(circleBtn)
     }
 
     override func didReceiveMemoryWarning() {
