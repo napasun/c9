933da3390f52b84d232faf306bf2b0c2fb3d94b0
napasun <wkzkfmxk23@gmail.com>
2019-02-01 17:50:02 +0900

    3장 공부

--- python/book02/ch3/mysite/polls/admin.py
+++ python/book02/ch3/mysite/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from polls.models import Question, Choice
 
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):  #admin.StackedInline
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline] # Choice 모델 클래스 같이 보기
+    list_display = ('question_text', 'pub_date')    #리스트 컬럼 항목
+    list_filter = ['pub_date']  #필터
+    search_fields = ['question_text']
+    #fields = ['pub_date', 'question_text'] #필드 순서 변경
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
