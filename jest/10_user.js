e24ba6b0b8debf4d104d6c22f08604f556913a08
napasun <wkzkfmxk23@gmail.com>
2019-04-18 10:02:31 +0900

    file delete

--- jest/10_user.js
+++
@@ -1,5 +0,0 @@
-const request = require("./10_request")
-
-exports.getUserName = function (userID) {
-  return request.request('/users/' + userID).then(user => user.name);
-}
--- jest/5_strings.spec.js
+++
@@ -1,7 +0,0 @@
-test('there is no I in them', () => {
-    expect('team').not.toMatch(/I/);
-})
-
-test('but there is a "shop" in christoph', () =>{
-    expect('Christoph').toMatch(/stop/)
-})
