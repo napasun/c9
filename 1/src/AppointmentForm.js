9c380d666fbebec287c8e38faca9cb23ae7c1eef
김형진 <wkzkfmxk12@illunex.com>
2019-09-11 16:37:37 +0900

    공부중

--- 1/src/AppointmentForm.js
+++ 1/src/AppointmentForm.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-export const AppointmentForm = () => (
+export const AppointmentForm = ({ selectableServices }) => (
     <form id="appointment">
-        <select name="service" />
+        <select name="service" value={service} readOnly>
+            <option />
+            {selectableServices.map(s => (
+                <option key={s}>{s}</option>
+            ))}
+            </select>
     </form>
 );
+
+AppointmentForm.defaultProps = {    //테스트 안함 정적 데이터가 아닌 동작을 테스트
+    selectableServices: [
+      'Cut',
+      'Blow-dry',
+      'Cut & color',
+      'Beard trim',
+      'Cut & beard trim',
+      'Extensions']
+  };
--- 1/test/AppointmentForm.test.js
+++ 1/test/AppointmentForm.test.js
@@ -17,13 +17,63 @@ describe('AppointmentForm', () => {
         expect(form('appointment')).not.toBeNull();
     });
 
+    const field = name => form('appointment').elements[name];
+
     describe('service field', () => {
+        const findOption = (dropdownNode, textContent) => {
+            const options = Array.from(dropdownNode.childNodes);
+            return options.find(
+              option => option.textContent === textContent
+            );
+        };
+
+
         it('renders as a select box', () => {
             render(<AppointmentForm />);
-            expect(form('appointment').elements.service)
-              .not.toBeNull();
-            expect(form('appointment').elements.service.tagName)
-              .toEqual('SELECT');
+            expect(field('service')).not.toBeNull();
+            expect(field('service').tagName).toEqual('SELECT');
+        });
+
+        it('초기 선택된 값이 비어 있는 지 확인', () => {
+            render(<AppointmentForm />);
+            const firstNode = field('service').childNodes[0];
+            expect(firstNode.value).toEqual('');
+            expect(firstNode.selected).toBeTruthy();
+          });
+
+        it('lists all salon services', () => {
+            const selectableServices = ['Cut', 'Blow-dry'];
+            
+            render(
+                <AppointmentForm
+                    selectableServices={selectableServices}
+                />
+            );
+            
+            const optionNodes = Array.from(
+                field('service').childNodes
+            );
+            const renderedServices = optionNodes.map(
+                node => node.textContent
+            );
+            expect(renderedServices).toEqual(
+                expect.arrayContaining(selectableServices)
+            );
+        });
+
+        it('pre-selects the existing value', () => {
+            const services = ['Cut', 'Blow-dry'];
+            render(
+              <AppointmentForm
+                selectableServices={services}
+                service="Blow-dry"
+              />
+            );
+            const option = findOption(
+              field('service'),
+              'Blow-dry'
+            );
+            expect(option.selected).toBeTruthy();
         });
     });
 });
