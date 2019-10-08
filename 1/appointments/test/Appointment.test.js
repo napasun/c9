f214eff1dde39430368200ced8b4edcd0ef95142
김형진 <wkzkfmxk12@illunex.com>
2019-10-08 18:16:39 +0900

    1장 공부 끝 2장 시작

--- 1/appointments/test/Appointment.test.js
+++ 1/appointments/test/Appointment.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Appointment} from '../src/Appointment'
+import {Appointment, AppointmentsDayView} from '../src/Appointment'
+import ReactTestUtils from 'react-dom/test-utils';
 
 describe('AppointmentsDayView', () => {
     let container;
@@ -11,10 +12,54 @@ describe('AppointmentsDayView', () => {
 
     const render = component => ReactDOM.render(component, container);
 
-    it('renders a div with the right id', () => {
+    it('div에 해당하는 ID를 가진 요소가 있는 지 확', () => {
         render(<AppointmentsDayView appointments={[]} />);
         //appointmentsDayView 이라는 ID값을 가진 요소가 있는 지 확인한다.
         expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    });
+
+    const today = new Date();
+    const appointments = [
+        {
+            startsAt: today.setHours(12, 0),
+            customer: { firstName: 'Ashley' }
+        },
+        {
+            startsAt: today.setHours(13, 0),
+            customer: { firstName: 'Jordan' }
+        }
+    ];
+
+    it('renders multiple appointments in an ol element', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelector('ol')).not.toBeNull();   //ol 요소가 있는 지?
+        // ol 요소 자식으로 2개가 있는 지
+        expect(container.querySelector('ol').children).toHaveLength(2);
+        expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+        expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+    });
+
+    it('값이 없을 경우, 나와야 되는 메시지', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(container.textContent).toMatch('There are no appointments scheduled for today.');
+    });
+
+    it('selects the first appointment by default', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.textContent).toMatch('Ashley');
+    });
+
+    it('li 요소 안에 button이 있다.', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll('li > button')).toHaveLength(2);
+        expect(container.querySelectorAll('li > button')[0].type).toEqual('button');
+    });
+
+    it.skip('선택된 텍스트가 나오는 지 확인', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        const button = container.querySelectorAll('button')[1];
+        ReactTestUtils.Simulate.click(button);
+        expect(container.textContent).toMatch('Jordan');
     });
 });
 
@@ -47,10 +92,10 @@ describe('Appointment', () => {
         const component = <Appointment customer={customer} />;
         const container = document.createElement('div');    //div 태그를 만든다.
         document.body.appendChild(container);       //div 태그를 body에 넣는 다.
-        //ReactDOM.render(component, container) component를 가져와서 render를 수행하고 
+        //ReactDOM.render(component, container) component를 가져와서 render를 수행하고
         //기존 DOM노드를 새로 렌더링 된 노드트리로 바꾼다. 대체되는 DOM노드를 container에서 가져온다.
         ReactDOM.render(component, container);
-      
+
         expect(document.body.textContent).toMatch('Ashley');
     });
 
@@ -59,9 +104,9 @@ describe('Appointment', () => {
         const component = <Appointment customer={customer} />;
         const container = document.createElement('div');
         document.body.appendChild(container);
-        
+
         ReactDOM.render(component, container);
-        
+
         expect(document.body.textContent).toMatch('Jordan');
     });
 });
