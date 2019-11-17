630cd400b93a15f4252026fc8960f506b28c2b1c
napasun <wkzkfmxk23@gmail.com>
2019-11-18 01:31:34 +0900

    swagger 스프링 문서화

--- spring/1/spring_1_5th/src/main/java/study/spring_1_5th/controller/TodoController.java
+++ spring/1/spring_1_5th/src/main/java/study/spring_1_5th/controller/TodoController.java
@@ -1,8 +1,9 @@
 package study.spring_1_5th.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,35 +15,53 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 
 @RestController
 public class TodoController {
     @Autowired private TodoService todoService;
 
+    @ApiOperation( //p283
+            value = "이름으로 모든 todos 검색",     //요약
+            notes = "일치하는 todos 리스트가 반환, 페이징은 지원안함", //설명
+            response = Todo.class,
+            responseContainer = "List",
+            produces = "application/json")
     @GetMapping("/user/{name}/todos")
     public List<Todo> retrieveTodos(@PathVariable String name) {
         return todoService.retrieveTodos(name);
     }
 
     @GetMapping(path = "/users/{name}/todos/{id}")
-    public EntityModel<Todo> retrieveTodo(@PathVariable String name, @PathVariable int id) {
+    public Resource<Todo> retrieveTodo(@PathVariable String name, @PathVariable int id) {
         Todo todo = todoService.retrieveTodo(id);
         if (todo == null) {
             throw new TodoNotFoundException("Todo Not Found");
         }
-        //Resouces => EntityModel
-        EntityModel<Todo> todoResource = new EntityModel<Todo>(todo);
+
+        Resource<Todo> todoResource = new Resource<Todo>(todo);
         /**
          * {"id":1,"user":"Jack","desc":"Learn Spring MVC","targetDate":1573892007908,"done":false,"_links":{"parent":{"href":"http://localhost:8080/users/Jack/todos"}}}
          * */
-        // ControllerLinkBuilder는 server.mvc로 이동, 사용되지 않음 경고표시, WebMvcLinkBuilder로 대체됨.
-        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveTodos(name));		//현재 클래스에서 retrieveTodos 메서드에 대한 링크를 가져온다.
+        ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveTodos(name));		//현재 클래스에서 retrieveTodos 메서드에 대한 링크를 가져온다.
         todoResource.add(linkTo.withRel("parent"));		//현재 자원과의 관계는 parent 이다.
         return todoResource;
     }
+//    @GetMapping(path = "/users/{name}/todos/{id}")
+//    public EntityModel<Todo> retrieveTodo(@PathVariable String name, @PathVariable int id) {
+//        Todo todo = todoService.retrieveTodo(id);
+//        if (todo == null) {
+//            throw new TodoNotFoundException("Todo Not Found");
+//        }
+//        //Resouces => EntityModel
+//        EntityModel<Todo> todoResource = new EntityModel<Todo>(todo);
+//        /**
+//         * {"id":1,"user":"Jack","desc":"Learn Spring MVC","targetDate":1573892007908,"done":false,"_links":{"parent":{"href":"http://localhost:8080/users/Jack/todos"}}}
+//         * */
+//        // ControllerLinkBuilder는 server.mvc로 이동, 사용되지 않음 경고표시, WebMvcLinkBuilder로 대체됨.
+//        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveTodos(name));		//현재 클래스에서 retrieveTodos 메서드에 대한 링크를 가져온다.
+//        todoResource.add(linkTo.withRel("parent"));		//현재 자원과의 관계는 parent 이다.
+//        return todoResource;
+//    }
 
     @RequestMapping(method = RequestMethod.POST, path = "/users/{name}/todos")
     ResponseEntity<?> add(@PathVariable String name, @Valid @RequestBody Todo todo) {
