ff9d0b7ea5adcdf2a626ba1eeb0ab40d71fc1cb3
napasun <wkzkfmxk23@gmail.com>
2019-11-20 02:09:06 +0900

    로케일

--- spring/1/spring_1_5th/src/main/java/study/spring_1_5th/Application.java
+++ spring/1/spring_1_5th/src/main/java/study/spring_1_5th/Application.java
@@ -3,8 +3,13 @@ package study.spring_1_5th;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 @SpringBootApplication
 public class Application {
@@ -16,5 +21,19 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);       // 자동으로 정의된 빈
         }
+    }
+    //국제화
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);      //로케일 설정
+        return sessionLocaleResolver;
+    }
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("message");      //메시지 소스의 기본 이름을 설정한다.
+        messageSource.setUseCodeAsDefaultMessage(true); //메시지를 찾을 수 없는 경우 기본 메시지를 반환한다.
+        return messageSource;
     }
 }
--- spring/1/spring_1_5th/src/main/java/study/spring_1_5th/controller/BasicController.java
+++ spring/1/spring_1_5th/src/main/java/study/spring_1_5th/controller/BasicController.java
@@ -1,12 +1,20 @@
 package study.spring_1_5th.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import study.spring_1_5th.bean.WelcomeBean;
 
+import java.util.Locale;
+
 @RestController
 public class BasicController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Hello";
@@ -22,5 +30,10 @@ public class BasicController {
     @GetMapping("/welcome-with-parameter/name/{name}")      //경로변수
     public WelcomeBean welcomeWithParameter(@PathVariable String name) {
         return new WelcomeBean(String.format(helloWorldTemplate, name));
+    }
+
+    @GetMapping("/welcome-internationalized")
+    public String msg(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {      //로케일은 요청헤더 Accept-Language에서 선택된다. 지정하지 않으면 기본 로케일이 사용된다.
+        return messageSource.getMessage("welcome.message", null, locale);
     }
 }
