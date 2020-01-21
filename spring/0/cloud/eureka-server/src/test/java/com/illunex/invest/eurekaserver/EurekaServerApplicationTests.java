5ef1f64acecde88853d5699543a855df28fbbcb9
napasun <wkzkfmxk23@gmail.com>
2020-01-21 18:57:07 +0900

    docker 설정

---
+++ spring/0/cloud/eureka-server/src/test/java/com/illunex/invest/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.illunex.invest.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
